1f0264fd01649e92869a8074f5707eb8c23c3083
Jose Arias <joseariaspantoj@gmail.com>
2024-04-18 23:22:23 -0500

    Agregue el crear nuevo libro desde postman pero no se guarda

--- src/main/java/com/example/obrestdatajpa/controller/BookController.java
+++ src/main/java/com/example/obrestdatajpa/controller/BookController.java
@@ -2,10 +2,11 @@ package com.example.obrestdatajpa.controller;
 
 import com.example.obrestdatajpa.entities.Book;
 import com.example.obrestdatajpa.repository.BookRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class BookController {
@@ -27,10 +28,32 @@ public class BookController {
         return bookRepository.findAll();
     }
     //Buscar un solo libro en base de datos
-    //public Book findOneById(Long id){
 
-    //}
+    /**
+     * request
+     * respone
+     * @param id
+     * @return
+     */
+    @GetMapping("/api/books/{id}")
+    public ResponseEntity <Book> findOneById(@PathVariable Long id){
+        Optional<Book> booOpt= bookRepository.findById(id);
+       //Opcion 1
+
+        if (booOpt.isPresent()){
+            return ResponseEntity.ok(booOpt.get());
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+        //opcion 2 = hace lo mismo que la primera opcion
+        //return booOpt.orElse(null);
+      }
     //crear un nuevo libro en base de datos
+    @PostMapping("/api/books")
+    public Book create(@RequestBody Book book){
+        //Guardar libro recibido por parametro en la base de datos
+        return bookRepository.save(book);
+    }
     //actualizar un libro existente en base de datos
     //borrar un libro en base de datos
 
